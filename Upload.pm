97255c0e6a696b0c29091067272d86802ce36d1f
Christophe Beauregard <cpb@cpan.org>
2004-12-14 18:06:02 +0000

    Import of CPB/Flickr-Upload-1.10 from CPAN.
    gitpan-cpan-distribution: Flickr-Upload
    gitpan-cpan-version:      1.10
    gitpan-cpan-path:         CPB/Flickr-Upload-1.10.tar.gz
    gitpan-cpan-author:       CPB
    gitpan-cpan-maturity:     released

--- Upload.pm
+++ Upload.pm
@@ -29,7 +29,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 );
 
-our $VERSION = do { my @r = (q$Revision: 1.9 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+our $VERSION = do { my @r = (q$Revision: 1.10 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 # Preloaded methods go here.
 
